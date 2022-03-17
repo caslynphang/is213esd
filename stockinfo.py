fe05f72313c4bbdaac0e2cc053e5b98fd05a23f4
snoozehao <91402038+snoozehao@users.noreply.github.com>
2022-03-17 21:49:17 +0800

    Update stockinfo.py

--- stockinfo.py
+++ stockinfo.py
@@ -10,11 +10,7 @@ yesterday = str(date.today() - timedelta(days=1))
 app = Flask(__name__)
 
 @app.route("/stock_info/buy/<string:stockName>")
-def buy(stockName):
-
-# recommend by close price
-
-    
+def buy(stockName):   
 # get yesterday's/today's close prices for all stocks (USE YOUR OWN KEYS LOL[i only hv 5 req/min] -- polygon.io)
     r = requests.get('https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/'+ yesterday +'?adjusted=true&apiKey=JVUOJpz7eTK1LXR6J0bZxnQVnyifIbvt')
     results = r.json()['results']
