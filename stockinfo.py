35c0cdaf9ab83a5ea79804b52b80bb33cc56ab42
snoozehao <91402038+snoozehao@users.noreply.github.com>
2022-04-04 02:50:08 +0800

    changed date so weekend will run fri insted

--- stockinfo.py
+++ stockinfo.py
@@ -1,12 +1,18 @@
 from flask import Flask, jsonify
 from datetime import date, timedelta
+import datetime
 import requests
 import json
 from flask_cors import CORS
 
-# yesterday / today->(api may not update that quickly :|)
-yesterday = str(date.today() - timedelta(days=1))
-# today = str(date.today())
+# if saturday and sunday return fridays' prices, else return today's prices
+weekno = datetime.datetime.today().weekday()
+if weekno == 5:
+    mostrecentday = str(date.today() - timedelta(days=1))
+elif weekno == 6:
+    mostrecentday = str(date.today() - timedelta(days=2))
+else:
+    mostrecentday = str(datetime.date.today()- timedelta(days=6))
 
 app = Flask(__name__)
 CORS(app)
@@ -14,9 +20,9 @@ CORS(app)
 @app.route("/stock_info/buy/<string:stockName>")
 def buy(stockName):   
 # get yesterday's/today's close prices for all stocks (USE YOUR OWN KEYS LOL[i only hv 5 req/min] -- polygon.io)
-    r = requests.get('https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/'+yesterday+'?adjusted=true&apiKey=')
+    r = requests.get('https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/'+mostrecentday+'?adjusted=true&apiKey=JVUOJpz7eTK1LXR6J0bZxnQVnyifIbvt')
     results = r.json()['results']
-    
+
 # find close price of stock chosen by user
     for result in results:
         if result['T'] == stockName:
@@ -27,10 +33,10 @@ def buy(stockName):
 
 
 
-@app.route("/stock_info/get_all_stock_info/<string:stockName>")
-def get_all_stock_info(stockName):
+@app.route("/stock_info/get_all_stock_info/")
+def get_all_stock_info():
 # get yesterday's/today's close prices for all stocks (USE YOUR OWN KEYS LOL[i only hv 5 req/min] -- polygon.io)
-    r = requests.get('https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2022-03-25?adjusted=true&apiKey=')
+    r = requests.get('https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/'+mostrecentday+'?adjusted=true&apiKey=JVUOJpz7eTK1LXR6J0bZxnQVnyifIbvt')
     results = r.json()['results']
     return results
 
